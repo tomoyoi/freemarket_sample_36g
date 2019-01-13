87aa558bb66174779f6caf5c03ceea5dcac51366
尾々野聖 <keks564976@gmail.com>
2019-01-13 17:46:23 +0900

    Delete no effect sync

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  # root 'items#pconfirm'
-  # root 'items#sell'
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'static_pages/signup'
   get 'static_pages/login'
   get 'static_pages/registration'
