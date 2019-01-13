631411092c64af587e03ff5305ebf0e7e670ffc7
NaokoShindo <naokoshindoh@icloud.com>
2019-01-13 16:38:08 +0900

    Edit-create-item-detail-page
    ＊indexはRenderしている部分を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # root 'welcome#index'
-  root 'items#pconfirm'
+  # root 'items#pconfirm'
   # root 'items#sell'
+  root 'items#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
