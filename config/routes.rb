98b66f4d122db466b8b0196a8234a98f4eee3ae8
尾々野聖 <keks564976@gmail.com>
2019-01-13 17:44:58 +0900

    Rewirte routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   # root 'items#pconfirm'
   # root 'items#sell'
-  root 'items#show'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'static_pages/signup'
   get 'static_pages/login'
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   get 'static_pages/deliver_address'
   get 'static_pages/payment'
   get 'static_pages/complite'
-  root 'welcome#index'
+  get 'users/mypage'
+  get 'items/sell'
+  get 'items/show'
+  get 'items/pconfirm'
 end
