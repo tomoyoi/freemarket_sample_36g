2393ee678b3806b9770526b8ec22a0e852b2f620
NaokoShindo <naokoshindoh@icloud.com>
2019-01-20 13:41:06 +0900

    create_images_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_034427) do
+ActiveRecord::Schema.define(version: 2019_01_20_043204) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_01_20_034427) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "images", "items"
 end
