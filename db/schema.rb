71a8aeb1758557b50d1c15638177aa61acfb63fa
NaokoShindo <naokoshindoh@icloud.com>
2019-01-20 16:21:51 +0900

    add_reference_column_to_images

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_064642) do
+ActiveRecord::Schema.define(version: 2019_01_20_071824) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.bigint "user_id"
     t.index ["item_id"], name: "index_images_on_item_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2019_01_20_064642) do
   end
 
   add_foreign_key "images", "items"
+  add_foreign_key "images", "users"
 end
