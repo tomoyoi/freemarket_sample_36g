d35b478cc04b7d4bcf6d10b527f273701df626bc
NaokoShindo <naokoshindoh@icloud.com>
2019-01-20 16:51:14 +0900

    create_identification_and_payment_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_071824) do
+ActiveRecord::Schema.define(version: 2019_01_20_074601) do
+
+  create_table "identifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "lastname", null: false
+    t.string "firstname", null: false
+    t.string "lastname_kana", null: false
+    t.string "firstname_kana", null: false
+    t.string "postcode", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "birthday", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identifications_on_user_id"
+  end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
@@ -31,6 +47,17 @@ ActiveRecord::Schema.define(version: 2019_01_20_071824) do
     t.string "size", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "payments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "card_number", null: false
+    t.string "expire_month", null: false
+    t.string "expire_year", null: false
+    t.string "security_code", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,6 +84,8 @@ ActiveRecord::Schema.define(version: 2019_01_20_071824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "identifications", "users"
   add_foreign_key "images", "items"
   add_foreign_key "images", "users"
+  add_foreign_key "payments", "users"
 end
