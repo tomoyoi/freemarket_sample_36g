96934263e1b8c570ac0edb98b8d9b79fdac57444
尾々野聖 <keks564976@gmail.com>
2019-01-30 11:11:09 +0900

    Set db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+     Item.create(
+        name: Faker::Book.title,
+        images_attributes: [image: "https://4.bp.blogspot.com/-2t-ECy35d50/UPzH73UAg3I/AAAAAAAAKz4/OJZ0yCVaRbU/s1600/book.png"],
+        price: 600,
+        description: "goood!",
+        delivery_fee: Faker::Boolean.boolean,
+        area: Faker::Boolean.boolean,
+        size: Faker::Boolean.boolean,
+      )
+end
