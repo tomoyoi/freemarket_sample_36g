81d09712e5b75afadafedf6ecfb2669ed5d4f75e
尾々野聖 <keks564976@gmail.com>
2019-02-11 16:54:21 +0900

    Signup action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def confirm
+    @user = User.new(signup_params)
+
+    if @user.valid?
+      render :action => 'confirm'
+    else
+     redirect_to :new
+    end
+  end
+
+
+  private
+
+  def signup_params
+    params.require(:user).permit(
+      :nickname,
+      :email,
+      :password,
+      :password_confirmation,
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :prefecture,
+      :city,
+      :postcode,
+      :house_number,
+      :building,
+      :tel_number,
+    )
+  end
+
+
 end
