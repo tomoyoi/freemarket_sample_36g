11701fe3c1b56cf896747e1fcf622aec0ffac282
尾々野聖 <keks564976@gmail.com>
2019-02-13 15:20:15 +0900

    Gemfile & schema

--- db/schema.rb
+++ db/schema.rb
@@ -112,24 +112,12 @@ ActiveRecord::Schema.define(version: 2019_01_20_115041) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postcode", null: false
-    t.string "prefecture", null: false
-    t.string "city", null: false
-    t.string "house_number", null: false
-    t.string "building", null: false
-    t.string "tel_number", null: false
     t.text "profile", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "identifications", "users"
