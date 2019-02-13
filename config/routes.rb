7fb54841a485d649083c8d4c94d7a0b0b49a12c7
尾々野聖 <keks564976@gmail.com>
2019-02-13 15:19:31 +0900

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, :controller => {
-    :registration => 'signup/registrations'
-  }
-
-  devise_scope :user do
-   get 'signup' => 'signup/registrations#index'
-   get 'signup/registration' => 'signup/registrations#new'
-   post 'signup/address' => 'signup/registrations#address'
-   post 'signup/complite' => 'sifgnup/registrations#complete'
-   post 'signup/complite' => 'signup/registrations#complite'
-  end
+  devise_for :users
 
   root 'items#index'
-  get 'static_pages/signup'
-  get 'static_pages/login'
-  get 'static_pages/registration'
-  get 'static_pages/deliver_address'
-  get 'static_pages/payment'
-  get 'static_pages/complite'
-  get 'static_pages/logout'
   get 'users/mypage'
   get 'users/payment'
   get 'users/profile'
   get 'users/identification'
   get 'users/mypage-edit'
+  resources :users, only: [:index, :new]
+  post 'users/confirm' => 'users#confirm'
   get 'items/sell'
   get 'items/show'
   get 'items/pconfirm'
