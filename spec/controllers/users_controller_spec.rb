a099ff261a5b9454b1ce6e56d6be0d0eaae33e03
尾々野聖 <keks564976@gmail.com>
2019-02-13 15:22:27 +0900

    Delete comment outs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
-#require 'spec_helper'
 
 describe UsersController do
 
-  # setup do
-  #   @request.env["devise.mapping"] = Devise.mappings[:user]
-  # end
-
   describe '#index' do
 
-    #subject {get '/signup'}
     before { get :index }
 
     it 'has a 200 status code' do
-      #request.env['PATH_INFO'] = 'signup'
       expect(response).to have_http_status(:ok)
-      #binding.pry
-      #is_expected.to be_success
-      #expect(:get => "/").to route_to(:controller => "welcome")
     end
 
     it 'renders the :index template' do
@@ -42,44 +32,26 @@ describe UsersController do
   end
 
   describe '#confirm' do
-    #let(:user) { build(:user) }
+
     before do
       post :confirm, params: { user: attributes_for(:user) }
       @user = User.new(request.params["user"])
     end
-    #let(:user) { create(:user) }
 
     it 'has a 200 status code' do
-      #post :confirm, params: { user: attributes_for(:user)}
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the :confirm template' do
-      #post :confirm, params: { user: attributes_for(:user)}
       expect(response).to render_template :confirm
     end
 
     it 'assigns @user' do
-      #post :confirm, params: { user: @user}
-      #binding.pry
       id = 1
       @user.id = id
       assigns(:user).id = id
       expect(assigns(:user)).to eq @user
     end
   end
-
-  # describe '#create' do
-
-  #   it 'has a 200 status code' do
-  #     post 'signup/create', params: { user: attributes_for(:user)}
-  #     expect(response).to have_http_status(:ok)
-  #   end
-
-  #   it 'renders the :complete template' do
-  #     post 'signup/create', params: { user: attributes_for(:user)}
-  #     expect(response).to render_template :complete
-  #   end
-  # end
 
 end
