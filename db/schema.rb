a036c23900790d49195196a770b3a508dd5fa3af
NaokoShindo <naokoshindoh@icloud.com>
2019-02-17 15:17:42 +0900

    add_brand_again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_055347) do
+ActiveRecord::Schema.define(version: 2019_02_17_061453) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "parent_id"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_02_09_055347) do
     t.string "standard_shipping_time"
     t.bigint "seller_id"
     t.bigint "buyer_id"
+    t.string "brand"
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
     t.index ["seller_id"], name: "index_items_on_seller_id"
   end
@@ -119,22 +120,24 @@ ActiveRecord::Schema.define(version: 2019_02_09_055347) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postcode", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building", null: false
+    t.string "tel_number", null: false
+    t.text "profile", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "last_name_kana", default: "", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.string "postcode", default: "", null: false
-    t.string "prefecture", default: "", null: false
-    t.string "city", default: "", null: false
-    t.string "house_number", default: "", null: false
-    t.string "building", default: "", null: false
-    t.string "tel_number", limit: 11, default: "", null: false
-    t.text "profile", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "identifications", "users"
