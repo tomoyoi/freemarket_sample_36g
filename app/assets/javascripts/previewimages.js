730b954ef5a2378a3014aa527bc50053412da853
尾々野聖 <keks564976@gmail.com>
2019-03-15 22:25:59 +0900

    Delete comment out

--- app/assets/javascripts/previewimages.js
+++ app/assets/javascripts/previewimages.js
@@ -1,45 +1,3 @@
-// $(document).on("turbolinks:load", function(){
-
-// //ドロップした時の処理にプレビューを表示させる
-// window.addEventListener('drop', function(event) {
-//    event.preventDefault();
-//    console.log("event")
-//    });
-
-// //ドラッグを止める
-//   window.addEventListener('dragover', function(event) {
-//        event.preventDefault();
-//    }, false);
-// });
-
-
-// $(function() {
-//   $("#drop").on({
-//     "dragenter dragover":function(e){
-//       e.preventDefault();
-//     },
-//     "drop":function(e){
-//       var file = e.originalEvent.dataTransfer.files[0];
-//       var fr1 = new FileReader();
-//       fr1.onload = function(e) {
-//         var blob=new Blob([e.target.result],{"type":file.type});
-//         var fr2 = new FileReader();
-//         fr2.onload = function(e) {
-//           var src=e.target.result;
-//           src=new Uint8Array(src);
-//           src=String.fromCharCode.apply("",src);
-//           src=btoa(src);
-//           src="data:"+file.type+";base64,"+src;
-//           $("<img>").attr({"src":src,"alt":file.name}).appendTo('#drop');
-//         }
-//         fr2.readAsArrayBuffer(blob);
-//       }
-//       fr1.readAsArrayBuffer(file);
-//       e.preventDefault();
-//     },
-//   });
-// });
-
 $(function() {
 
   var upload_image = $(".sell-dropbox-items");
@@ -56,9 +14,9 @@ $(function() {
                     <a class="sell-dropbox-items-btn sell-image_delete" id=${label_num - 1}>
                       <p>削除</p>
                     </a>
-                </li>`
+                </li>`;
     upload_image.append(html);
-    ImageCount()
+    ImageCount();
   }
 
   // 削除機能
